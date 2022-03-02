d075d5777a307fd16c4928a49f7b85fc9de1cc1c
chanda chewe <chewec03@gmail.com>
2022-03-02 11:50:25 -0800

    Showing Errors if user Credentials does not match the records in the database

--- header.php
+++ header.php
@@ -9,6 +9,22 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>Sign In </title>
 
+ <!-- jQuery library -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<!--Bootstrap-->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+
+
+ <!--Fontawsome--> 
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <!--End fontawesome-->
+
+
+
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 	include_once 'header.php';
 ?>
 <?php
+
 	/*if(!empty($_SESSION['employee_id'])){
 		header("location:./dashboard.php");
 	}*/
-	
-	if(!empty($_POST['username']) && !empty($_POST['password'])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
+          
+ 
+            $user_name =$_POST['username'];
+            $password = $_POST['password'];
 
-		$user_name =$_POST['username'];
-		$password = $_POST['password'];
-
-		$check_query = "
+            $check_query = "
 			SELECT 
 				*
 			FROM ".TBL_EMPLOYEE."
@@ -20,29 +22,32 @@
 				Password='".$password."'
 		";
 
-		$db_obj = mysqli_query($connection_obj,$check_query);
+            $db_obj = mysqli_query($connection_obj, $check_query);
 
-		if(mysqli_num_rows($db_obj) > 0){
-			$employee_data = mysqli_fetch_assoc($db_obj);
-			if(!empty($employee_data) && is_array($employee_data) && count($employee_data) > 0){
-				$store_seesion = array(
+            if (mysqli_num_rows($db_obj) > 0) {
+                $employee_data = mysqli_fetch_assoc($db_obj);
+                if (!empty($employee_data) && is_array($employee_data) && count($employee_data) > 0) {
+                    $store_seesion = array(
                     "employee_email" => $employee_data['Email'],
-					"employee_id" => $employee_data['Employee_id'],
-					"rolecode" => $employee_data['Roles'],
+                    "employee_id" => $employee_data['Employee_id'],
+                    "rolecode" => $employee_data['Roles'],
                     "emp_name" => $employee_data['Employee_name']
                     
                    // "statuscode"=>$employee_data['Status']
-				);
-				
-				$_SESSION = $store_seesion;
-				header("location:./dashbord.php");
-			}else{
-				header("location:./index.php");
-			}
-		}else{
-			header("location:./index.php");
-		}
-	}
+                );
+            
+        
+                    $_SESSION = $store_seesion;
+                    header("location:./dashbord.php");
+                }
+            } else {
+                ## Session to store errors for wrong credentials
+                $_SESSION['wrong_credentials'] = 'These credentials do not match our records';
+                ## The following header is not needed as it will return to the same page
+                //header("location:./index.php");
+            }
+        }
+    }
 ?>
 <div class="login-box">
         <div class="logo">
@@ -51,8 +56,31 @@
         </div>  
         <div class="card">
             <div class="body">
-                <form id="sign_in" method="POST" action='.'>
-                    <div class="msg">Sign in to start your session</div>
+                <form id="sign_in" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <?php 
+## Check if the sessions for wrong credentials are stored 
+
+  if (isset($_SESSION['wrong_credentials'])) {
+## Show Errors Here if user Credentials does not match the records in the database
+      echo '<div class="alert alert-danger" role="alert">';
+      echo '<i class="fa-regular fa-bell"></i>';
+      echo '<strong> Hello there!</strong> You have some feedbacks,';
+       
+      echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+      echo '<span aria-hidden="true">&times;</span>';
+      echo '</button>';
+## Show Sessions here      
+      echo $_SESSION['wrong_credentials'];    
+## destroy the session
+      session_destroy();
+      echo '</div>';
+## Validation Errors for wrong Credentials Ends Here
+  }
+
+    ?>
+
+
+                <div class="msg">Sign in to start your session</div>
                     <div class="input-group">
                         <span class="input-group-addon">
                             <i class="material-icons">person</i>
@@ -88,3 +116,7 @@
 <?php
 	include_once 'footer.php';
 ?>
+
+
+            
+  
